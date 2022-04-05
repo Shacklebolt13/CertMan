36adf847593ab98b813a2c6660ac6c2fc81ef04e
Shacklebolt13 <grimmgagan@gmail.com>
2022-04-05 19:36:18 +0530

    Ready State

--- certMan.py
+++ certMan.py
@@ -3,99 +3,107 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import pandas as pd
-import smtplib,ssl,sys
-import locMan 
+import smtplib, ssl, sys
+import locMan
 
-#----------------- TO CUSTOMIZE-------------------
-eventSm="xyz"   #for files
-event="xyz Competion"   #for mail
-filedir="certs/" 
-makePdf=True
-msg=f"Thank You For Participating in {event}. Please find your certificate attatched to this mail."
-debugServerPort=1025
-mailSub=f"Certificate For {event}"
-#--------------------------------------------------
+# ----------------- TO CUSTOMIZE-------------------
+eventSm = " "  # for files
+event = "the 2 Day Training in Android App Development"  # for mail
+filedir = "certs/"
+makePdf = True
+msg = f"Thank You For Participating in {event}. Please find your certificate attatched to this mail."
+debugServerPort = 1025
+mailSub = f"Badhiyaan Ji"
+# --------------------------------------------------
 
-if("-d" not in sys.argv):
+if "-d" not in sys.argv:
     context = ssl.create_default_context()
-    sender=sys.argv[1]
-    password=sys.argv[2]
-    server=smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) 
+    sender = sys.argv[1]
+    password = sys.argv[2]
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context)
+    print(sender, password)
     server.login(sender, password)
 else:
-    sender="Test"
-    server=smtplib.SMTP("localhost",port=debugServerPort)
-#syntax: python3 ownAuto.py <gmail> <pass> [options]
+    sender = "Test"
+    server = smtplib.SMTP("localhost", port=debugServerPort)
+# syntax: python3 ownAuto.py <gmail> <pass> [options]
 # -v verbose
 # -d debug smtp server
-#-c custom input
-#-n no attatch file
-    
+# -c custom input
+# -n no attatch file
+
+
 def capUp(name):
-    chunks=name.split(' ')
-    return " ".join(list(map(lambda x: x[0].upper()+x[1:],chunks)))
-    
+    chunks = name.split(" ")
+    return " ".join(list(map(lambda x: x.capitalize(), chunks)))
+
+
 def main():
-    if("-c" not in sys.argv ):
-        df=pd.read_csv('participants.csv')
-        name_list=list(df['name'])
-        email_list=list(df['email'])
+    if "-c" not in sys.argv:
+        df = pd.read_excel("participants.xlsx")
+        name_list = list(df["name"])
+        email_list = list(df["mail"])
+        for *i, coun in zip(name_list, email_list, range(1, 100000)):
+            print(i, coun)
     else:
-        name_list=['p1','p2','p3']#  list(df['name'])
-        email_list=['a@emai1.com','b@email.com','c@email.com']
+        name_list = ["p1", "p2", "p3"]  #  list(df['name'])
+        email_list = ["a@emai1.com", "b@email.com", "c@email.com"]
 
-    for name,email in zip(name_list,email_list):
-        name=capUp(name)
-        filename=createPdf(name)
-        maillto(email,filename,name)
+    for name, email in zip(name_list, email_list):
+        name = capUp(name)
+        filename = createPdf(name)
+        maillto(email, filename, name)
 
 
+def maillto(address, filename, name):
 
-def maillto(address,filename,name):
-    
-    message=attachMsg(name,address)
+    message = attachMsg(name, address)
 
-    if("-n" not in sys.argv):
+    if "-n" not in sys.argv:
         message.attach(attachFile(filename))
-    
-    text=message.as_string()
 
-    if("-v" in sys.argv):
+    text = message.as_string()
+
+    if "-v" in sys.argv:
         print(text)
-    
-    
+
     server.sendmail(sender, address, text)
 
 
-
 def createPdf(name):
-    debugMode=True if("-v" in sys.argv ) else False
-    
-    return locMan.saveCert(name,debug=debugMode,pdf=makePdf)
+    debugMode = True if ("-v" in sys.argv) else False
 
-def attachMsg(name,address):
+    return locMan.saveCert(name, debug=debugMode, pdf=makePdf)
+
+
+def attachMsg(name, address):
 
     message = MIMEMultipart()
     message["From"] = sender
     message["To"] = address
     message["Subject"] = mailSub
 
-    finalmsg=f"Hi {name},\n"+msg
-    
-    message.attach(MIMEText(finalmsg, "plain"))    
+    # finalmsg = open("team_selection.html", "r").read()
+    finalmsg = f"Hi {name},\n" + msg
+
+    message.attach(MIMEText(finalmsg, "html"))
 
     return message
 
 
 def attachFile(filename):
     attchdFile = MIMEBase("application", "octet-stream")
-    
+
     with open(filename, "rb") as attachment:
         attchdFile.set_payload(attachment.read())
-    
+
     encoders.encode_base64(attchdFile)
-    attchdFile.add_header("Content-Disposition",f"attachment; filename= {filename}",)
+    attchdFile.add_header(
+        "Content-Disposition",
+        f"attachment; filename= {filename}",
+    )
 
     return attchdFile
+
 
 main()
--- locMan.py
+++ locMan.py
@@ -1,33 +1,34 @@
 from PIL import Image, ImageDraw, ImageFont
 
 
-def saveCert(name,template='template.jpg',Certfont='Elegante.ttf',debug=False,pdf=True):
-    
-    template='template.jpg'
-    Certfont='Elegante.ttf'
-    location=(220,430)
+def saveCert(
+    name, template="template.jpg", Certfont="Elegante.ttf", debug=False, pdf=True
+):
 
+    template = "template.jpg"
+    Certfont = "gsans.ttf"
+    location = (400, 480)
+    name = f"{name:^30}"
     image = Image.open(template)
     draw = ImageDraw.Draw(image)
     font = ImageFont.truetype(Certfont, size=70)
-    color = 'rgb(0, 0, 0)'
-    
-    
-    
+    color = "rgb(0, 230, 118)"
+
     draw.text(location, name, fill=color, font=font)
-    imageName = "certs/"+name
-    
-    if(pdf==True):     
-        image.convert('RGB')
-        imageName+=".pdf"
+    imageName = "certs/" + name.strip()
+
+    if pdf == True:
+        image.convert("RGB")
+        imageName += ".pdf"
         image.save(imageName)
     else:
-        imageName+=".jpg"
+        imageName += ".jpg"
         image.save(imageName)
-    
-    if(debug==True):
-        print("name:",name,"location:",location,"saved at:",imageName)
-    
+
+    if debug == True:
+        print("name:", name, "location:", location, "saved at:", imageName)
+
     return imageName
 
-saveCert("Test")
+
+saveCert("Test", pdf=False)
